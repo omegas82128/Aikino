f80dd3a013a17c3b289c596f1555c45aa19e3473
Haris <nobody82128@gmail.com>
2020-07-20 00:02:21 +0500

    added SettingsWindow, preferences, alert cleanup and searchWindow when posters not found or media not found

--- src/main/java/com/omegas/util/Preferences.kt
+++ src/main/java/com/omegas/util/Preferences.kt
@@ -3,7 +3,6 @@ package com.omegas.util
 import java.util.prefs.Preferences
 
 object Preferences {
-
     val preferences: Preferences = Preferences.userRoot().node(javaClass.name)
     var iconType: IconType = IconType.valueOf(preferences[Constants.ICON_TYPE_KEY,IconType.WITH_TEMPLATE.name])
     var hideIcon = preferences.getBoolean(Constants.HIDE_ICONS_KEY,true)
