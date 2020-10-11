f365b2aaae2ac6950bc8c1687e984d8f72f2430c
Haris <nobody82128@gmail.com>
2020-10-11 13:52:08 +0500

    updated app version

--- src/main/java/com/omegas/util/Constants.kt
+++ src/main/java/com/omegas/util/Constants.kt
@@ -11,7 +11,7 @@ import java.util.regex.Pattern
 object Constants{
 
     const val APP_NAME = "Aikino"
-    const val APP_VERSION = "2.0.8"
+    const val APP_VERSION = "2.0.9"
 
     //resources
     val PLACEHOLDER_IMAGE_PATH = javaClass.getResource("/images/placeholder.jpg").toString()
