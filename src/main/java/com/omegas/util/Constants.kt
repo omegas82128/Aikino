6b6461f4fdf5d5a2f6318ea406f17813deed1cb7
Haris <nobody82128@gmail.com>
2021-05-11 21:43:27 +0500

    updated dependencies

--- src/main/java/com/omegas/util/Constants.kt
+++ src/main/java/com/omegas/util/Constants.kt
@@ -13,7 +13,7 @@ import java.awt.Dimension
 object Constants {
 
     const val APP_NAME = "Aikino"
-    const val APP_VERSION = "2.6.0"
+    const val APP_VERSION = "2.6.1"
 
     val ICON = Image(javaClass.getResource("/icon.png")!!.toString())
     val NOT_FOUND_IMAGE = Image(javaClass.getResource("/images/image-not-found.png")!!.toString())
